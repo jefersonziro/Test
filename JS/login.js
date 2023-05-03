d98a5230c5a87bd83be26d2aa8d122e44186a6ed
jefersonziro <60409499+jefersonziro@users.noreply.github.com>
2023-05-03 10:06:29 +0100

    User request error

---
+++ JS/login.js
@@ -0,0 +1,50 @@
+var listaDeFuncionarios = [{
+    email: "emailum@mail.com",
+    password: "123",
+    name: "Joãozinho",
+    phone_number: "(55) 99939-3993",
+    role: "Desenvolvedor"
+}, {
+    email: "emaildois@mail.com",
+    password: "123",
+    name: "Mariazinha",
+    phone_number: "(53) 92329-3993",
+    role: "Gerente"
+}, {
+    email: "emailtres@mail.com",
+    password: "123",
+    name: "Terezinha",
+    phone_number: "(23) 88379-3993",
+    role: "Estagiário"
+}, ];
+
+function fazerLogin(emailDigitadoNoFormulario, senhaDigitadaNoFormulario) {
+
+    var usuarioValido = listaDeFuncionarios.find(function(funcionario, posicaoNaLista) {
+        return 
+            funcionario.email == emailDigitadoNoFormulario && 
+            funcionario.password == senhaDigitadaNoFormulario;
+    })
+
+    if (usuarioValido) {
+        alert("Bem vindo, Usuário foi encontrado");
+    } else {
+        alert("Usuário não encontrado!");
+    }
+}
+
+$(document).ready(function() {            
+
+$("#form-login").submit(function(event) {
+
+    event.preventDefault();
+
+    var emailDigitado = $("#inputEmail").val();
+
+    var senhaDigitada = $("#inputPassword").val();
+
+    fazerLogin(emailDigitado, senhaDigitada);
+    
+})
+
+})
